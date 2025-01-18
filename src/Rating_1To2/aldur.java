23bfb6e0ea9ddd68b0c14f21634f5ea41c0a170e
Nikolas Kolic <nikolas.kolic@gmail.com>
2025-01-18 17:12:28 +0100

    Adding the easy challange Aldur.

---
+++ src/Rating_1To2/aldur.java
@@ -0,0 +1,22 @@
+package Rating_1To2;
+
+import java.util.Scanner;
+
+public class aldur {
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        int numberOfFriends = myScanner.nextInt();
+        int currentYoungestAge = myScanner.nextInt();
+
+        for(int i = 1; i < numberOfFriends; i++){
+            int currentAge = myScanner.nextInt();
+
+            if(currentAge < currentYoungestAge){
+                currentYoungestAge = currentAge;
+            }
+        }
+
+        System.out.println(currentYoungestAge);
+    }
+
+}
