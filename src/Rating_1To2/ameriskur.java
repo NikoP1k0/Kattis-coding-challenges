92d6255bd39ca85b3bcbc7e8fbcf0123fbaa9c0d
Nikolas Kolic <nikolas.kolic@gmail.com>
2025-01-19 15:27:12 +0100

    Adding the easy challanges Alphabet spam and Ameriskur.

---
+++ src/Rating_1To2/ameriskur.java
@@ -0,0 +1,11 @@
+package Rating_1To2;
+
+import java.util.Scanner;
+
+public class ameriskur {
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        int measurement = myScanner.nextInt();
+        System.out.println(measurement * 0.09144);
+    }
+}
