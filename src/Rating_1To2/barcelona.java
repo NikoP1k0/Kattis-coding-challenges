9afb8a62dfe956a3673a8d710f55812fed896c11
Nikolas Kolic <nikolas.kolic@gmail.com>
2025-01-23 21:49:34 +0100

    Adding the easy challenge "Barcelona".

---
+++ src/Rating_1To2/barcelona.java
@@ -0,0 +1,34 @@
+package Rating_1To2;
+
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class barcelona {
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        int numberOfBags = myScanner.nextInt(); int BennisBagPosition = myScanner.nextInt();
+        int[] allBagPositions = new int[numberOfBags];
+
+        // Fetch all the bag positions
+        for(int i = 0; i < numberOfBags; i++){
+            allBagPositions[i] = myScanner.nextInt();
+        }
+
+        int position = 0;
+        for (int i = 0; i < numberOfBags; i++) {
+            if (allBagPositions[i] == BennisBagPosition) {
+                position = i + 1; // Convert to 1-based index
+                break;
+            }
+        }
+
+        if(position == 1){
+            System.out.println("fyrst");
+        } else if(position == 2){
+            System.out.println("naestfyrst");
+        } else{
+            System.out.println(position + " fyrst");
+        }
+
+    }
+}
