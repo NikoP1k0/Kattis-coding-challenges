9deb1444d9a950110d2e6849cf81ed8ede4da2b5
Nikolas Kolic <nikolas.kolic@gmail.com>
2025-01-28 22:09:00 +0100

    Adding 8 solutions of challenges rated "Easy" in the difficulty scale at the time of this commit.

---
+++ src/Easy/bela.java
@@ -0,0 +1,40 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class bela {
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        int hands = myScanner.nextInt();
+        char dominantSuit = myScanner.next().charAt(0);
+        myScanner.nextLine();
+
+        int totalPoints = 0;
+        for(int i = 0; i < hands * 4; i++){
+            String currentCard = myScanner.nextLine();
+            char currentNumber = currentCard.charAt(0);
+            char currentSuit = currentCard.charAt(1);
+
+            switch (currentNumber) {
+                case 'A' -> totalPoints += 11;
+                case 'K' -> totalPoints += 4;
+                case 'Q' -> totalPoints += 3;
+                case 'J' -> {
+                    if (currentSuit == dominantSuit) {
+                        totalPoints += 20;
+                    } else {
+                        totalPoints += 2;
+                    }
+                }
+                case 'T' -> totalPoints += 10;
+                case '9' -> {
+                    if (currentSuit == dominantSuit) {
+                        totalPoints += 14;
+                    }
+                }
+            }
+        }
+
+        System.out.println(totalPoints);
+    }
+}
---
+++ src/Easy/biladlyklabord.java
@@ -0,0 +1,20 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class biladlyklabord {
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        String sentence = myScanner.nextLine();
+        StringBuilder newSentence = new StringBuilder();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char currentLetter = sentence.charAt(i);
+
+            if (newSentence.length() == 0 || currentLetter != newSentence.charAt(newSentence.length() - 1)) {
+                newSentence.append(currentLetter);
+            }
+        }
+        System.out.println(newSentence);
+    }
+}
