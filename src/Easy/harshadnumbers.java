eabfb3fc11e00ec9622196738f0f8133a902a944
Nikolas Kolic <nikolas.kolic@gmail.com>
2025-02-03 22:05:25 +0100

    Adding 7 solutions of challenges rated "Easy" in the difficulty scale at the time of this commit.

---
+++ src/Easy/harshadnumbers.java
@@ -0,0 +1,27 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class harshadnumbers {
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        int number = myScanner.nextInt();
+
+        while(!isHarshadNumber(number)){
+            number++;
+        }
+
+        System.out.println(number);
+    }
+
+    public static boolean isHarshadNumber(int num){
+        String number = String.valueOf(num);
+
+        int sum = 0;
+        for(int i = 0; i < number.length(); i++){
+            sum += Integer.parseInt(String.valueOf(number.charAt(i)));
+        }
+
+        return num % sum == 0;
+    }
+}
---
+++ src/Easy/jobexpenses.java
@@ -0,0 +1,20 @@
+package Easy;
+
+import java.util.Scanner;
+
+public class jobexpenses {
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        int numberOfExpensesAndIncomes = myScanner.nextInt();
+        int expensesSum = 0;
+
+        for(int i = 0; i < numberOfExpensesAndIncomes; i++){
+            int number = myScanner.nextInt();
+            if(number < 0){
+                expensesSum += number;
+            }
+        }
+
+        System.out.println(expensesSum * -1);
+    }
+}
